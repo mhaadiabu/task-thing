24b48c3538f5f04fe56890c0c9c143f0d8646e6e
mhaadiabu <mhabubakar999@outlook.com>
2025-12-10 03:09:20 +0000

    chore: remove react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,22 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { scan } from "react-scan";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import './index.css';
 
 // Import the generated route tree
-import { routeTree } from "./routeTree.gen";
-
-scan({
-  enabled: import.meta.env.DEV,
-});
+import { routeTree } from './routeTree.gen';
 
 // Create a new router instance
 const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router;
   }
 }
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
