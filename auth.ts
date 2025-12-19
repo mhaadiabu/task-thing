5d7a01ef8196a2dbff53402bd144064eff5c4f2e
mhaadiabu <mhabubakar999@outlook.com>
2025-12-19 14:29:04 +0000

    Read allowed origins from ALLOWED_ORIGINS env

--- auth.ts
+++ auth.ts
@@ -4,12 +4,13 @@ import { db } from './server/db';
 import * as schema from './server/db/auth-schema';
 import 'dotenv/config';
 
-const BETTER_AUTH_URL = process.env.BETTER_AUTH_URL ?? 'http://localhost:8000';
+const BETTER_AUTH_URL = process.env.BETTER_AUTH_URL;
+const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(',') ?? ['http://localhost:5173'];
 
 export const auth = betterAuth({
   baseURL: BETTER_AUTH_URL,
-  allowedOrigins: ['http://localhost:5173', BETTER_AUTH_URL],
-  trustedOrigins: ['http://localhost:5173'],
+  allowedOrigins: [...ALLOWED_ORIGINS, BETTER_AUTH_URL],
+  trustedOrigins: ALLOWED_ORIGINS,
   advanced: {
     crossSubDomainCookies: {
       enabled: true,
