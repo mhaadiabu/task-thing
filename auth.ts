2a97efe865b8d9130dc10da292c45ad49b03b16c
mhaadiabu <mhabubakar999@outlook.com>
2025-12-23 20:55:08 +0000

    chore: filter out falsey origins and assert string[] type

--- auth.ts
+++ auth.ts
@@ -5,12 +5,18 @@ import * as schema from './server/db/auth-schema';
 import 'dotenv/config';
 
 const BETTER_AUTH_URL = process.env.BETTER_AUTH_URL;
-const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(',') ?? ['http://localhost:5173'];
+const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(',') ?? [
+  'http://localhost:5173',
+];
 
 export const auth = betterAuth({
   baseURL: BETTER_AUTH_URL,
-  allowedOrigins: [...ALLOWED_ORIGINS, BETTER_AUTH_URL],
-  trustedOrigins: ALLOWED_ORIGINS,
+  allowedOrigins: [...ALLOWED_ORIGINS, BETTER_AUTH_URL].filter(
+    Boolean,
+  ) as string[],
+  trustedOrigins: [...ALLOWED_ORIGINS, BETTER_AUTH_URL].filter(
+    Boolean,
+  ) as string[],
   advanced: {
     crossSubDomainCookies: {
       enabled: true,
