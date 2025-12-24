d8d1f6b8354e8397ca1bcb0a77b2569763b0183c
mhaadiabu <mhabubakar999@outlook.com>
2025-12-24 03:10:09 +0000

    Filter falsy origins and enable secure cookies

--- auth.ts
+++ auth.ts
@@ -5,15 +5,22 @@ import * as schema from './server/db/auth-schema';
 import 'dotenv/config';
 
 const BETTER_AUTH_URL = process.env.BETTER_AUTH_URL;
-const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(',') ?? ['http://localhost:5173'];
+const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(',') ?? [
+  'http://localhost:5173',
+];
 
 export const auth = betterAuth({
   baseURL: BETTER_AUTH_URL,
-  allowedOrigins: [...ALLOWED_ORIGINS, BETTER_AUTH_URL],
+  allowedOrigins: [...ALLOWED_ORIGINS, BETTER_AUTH_URL].filter(Boolean),
   trustedOrigins: ALLOWED_ORIGINS,
   advanced: {
     crossSubDomainCookies: {
       enabled: true,
+    },
+    useSecureCookies: true,
+    defaultCookieAttributes: {
+      sameSite: 'none' as const,
+      secure: true,
     },
   },
   database: drizzleAdapter(db, {
