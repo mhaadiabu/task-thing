6737a5b6ed87284d898a9f4c6af3726dd1bfedbf
mhaadiabu <mhabubakar999@outlook.com>
2025-12-25 11:07:54 +0000

    feat: create TanStack router with trpc and QueryClient

---
+++ src/router.tsx
@@ -0,0 +1,36 @@
+import { createRouter as createTanstackRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+import { queryClient, trpc } from './utils/trpc';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+export function createRouter() {
+  const router = createTanstackRouter({
+    routeTree,
+    scrollRestoration: true,
+    defaultPreload: 'intent',
+    context: {
+      trpc,
+      queryClient,
+    },
+    defaultPendingComponent: () => (
+      <div className='w-full h-full justify-center items-center'>
+        Loading ...
+      </div>
+    ),
+    Wrap: function WrapComponent({ children }) {
+      return (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      );
+    },
+  });
+
+  return router;
+}
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof createRouter>;
+  }
+}
