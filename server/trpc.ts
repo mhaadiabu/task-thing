2959cd9c9f05b48fc759caf494d7f49fbc3d7918
mhaadiabu <mhabubakar999@outlook.com>
2025-12-25 12:54:22 +0000

    feat: simplify tRPC context by removing headers

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,11 +1,6 @@
 import { initTRPC } from '@trpc/server';
-import type { IncomingHttpHeaders } from 'http';
 
-export type Context = {
-  headers: IncomingHttpHeaders;
-};
-
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
