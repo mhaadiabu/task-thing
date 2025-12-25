615bb8d886900120dfeffd75572fdf3f8797f5e4
mhaadiabu <mhabubakar999@outlook.com>
2025-12-25 15:44:07 +0000

    feat: use loading Spinner for default pending component

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createRouter as createTanstackRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import { queryClient, trpc } from './utils/trpc';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { Spinner } from '@/components/ui/spinner';
 
 export function createRouter() {
   const router = createTanstackRouter({
@@ -12,11 +13,7 @@ export function createRouter() {
       trpc,
       queryClient,
     },
-    defaultPendingComponent: () => (
-      <div className='w-full h-full justify-center items-center'>
-        Loading ...
-      </div>
-    ),
+    defaultPendingComponent: () => <Spinner className='size-8' />,
     Wrap: function WrapComponent({ children }) {
       return (
         <QueryClientProvider client={queryClient}>
