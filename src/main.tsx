ff314dcce94d7af4c7ae2b990a4dd3623bdba31c
mhaadiabu <mhabubakar999@outlook.com>
2025-12-25 11:30:11 +0000

    use local createRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { createRouter } from './router';
+import { RouterProvider } from '@tanstack/react-router';
 import './index.css';
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen';
+// import { routeTree } from './routeTree.gen';
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+// const router = createRouter({ routeTree });
 
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
-}
+// // Register the router instance for type safety
+// declare module '@tanstack/react-router' {
+//   interface Register {
+//     router: typeof router;
+//   }
+// }
+
+const router = createRouter();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
