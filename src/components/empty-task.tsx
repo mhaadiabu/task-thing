bb2d1b465a5a902ca46c3015c206869a910f3a98
mhaadiabu <mhabubakar999@outlook.com>
2026-02-03 20:29:24 +0000

    refactor: add empty task component

---
+++ src/components/empty-task.tsx
@@ -0,0 +1,39 @@
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty';
+import { Button } from './ui/button';
+import { Plus } from 'lucide-react';
+
+
+export const EmptyTask = ({
+  action,
+  icon,
+  title,
+  description,
+}: {
+  action?: () => void;
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}) => (
+  <Empty>
+    <EmptyHeader>
+      <EmptyMedia variant='icon'>{icon}</EmptyMedia>
+      <EmptyTitle>{title}</EmptyTitle>
+      <EmptyDescription>{description}</EmptyDescription>
+    </EmptyHeader>
+    <EmptyContent>
+      {title === 'No Tasks Created' && (
+        <Button size='sm' onClick={action}>
+          <Plus />
+          Create Task
+        </Button>
+      )}
+    </EmptyContent>
+  </Empty>
+);
