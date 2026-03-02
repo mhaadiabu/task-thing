8436bb1ae4b17550e51f144e0aa7a3c48c8d1319
mhaadiabu <mhabubakar999@outlook.com>
2026-03-02 15:44:03 +0000

    chore: update config and tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 import 'dotenv/config';
 
 // https://vite.dev/config/
